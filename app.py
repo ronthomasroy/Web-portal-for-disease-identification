98132d5210ec313fe8072da4ffa9d50587a7da31
Ron Thomas Roy <ronroy003@gmail.com>
2024-11-02 18:55:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -59,5 +59,6 @@ def predict():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
 
